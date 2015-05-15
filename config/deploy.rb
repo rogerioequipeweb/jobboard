1de6fa467971e93aab2b4d633f654d36c56323e4
rogerioequipeweb <rogerio.equipeweb@gmail.com>
2015-05-15 11:51:56 -0300

    TESTE

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ namespace :bundle do
 
   desc "run bundle install and ensure all gem requirements are met"
   task :install do
-    run "cd #{current_path} && bundle install  --without=test --no-update-sources"
+    run "cd #{current_path} && bundle install"
   end
 
 end
