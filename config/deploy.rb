84842cd6751ec2f28598b8c0cefc7e8dd26f68b1
rogerioequipeweb <rogerio.equipeweb@gmail.com>
2015-05-15 11:44:04 -0300

    testando

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,15 +34,15 @@ set :scm, :git
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+namespace :bundle do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  desc "run bundle install and ensure all gem requirements are met"
+  task :install do
+    run "cd #{current_path} && bundle install  --without=test --no-update-sources"
   end
 
 end
+before "deploy:restart", "bundle:install"
+
+
+    
